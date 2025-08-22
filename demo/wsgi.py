7fdd4aebfd9d1b6beeb6866e97f07c51bd34f8a3
Nivethithachandrakrishnan <141031571+Nivethithachandru@users.noreply.github.com>
2025-08-22 17:17:33 +0530

    Update wsgi.py

--- demo/wsgi.py
+++ demo/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "github_actions.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'github_actions.settings')
 
 application = get_wsgi_application()

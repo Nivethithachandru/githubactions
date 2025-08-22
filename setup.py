564610fb196569160203af6d16e4451889c0f553
Nivethithachandrakrishnan <141031571+Nivethithachandru@users.noreply.github.com>
2025-08-22 15:19:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ def get_version():
     version = {}
     with open("core/__init__.py") as f:  # or maybe demo/__init__.py
         exec(f.read(), version)
-    return version["__version__"]
+    return version[ "__version__" ] 

a223419842a99d4c00a446cf533ba0a48ce15f98
Nivethithachandrakrishnan <141031571+Nivethithachandru@users.noreply.github.com>
2025-08-22 13:33:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
+from setuptools import setup, find_packages
+
 def get_version():
     version = {}
-    with open("core/__init__.py") as f:
+    with open("core/__init__.py") as f:  # Use "demo/__init__.py" if using demo
         exec(f.read(), version)
-    return version["0.1.0"]
+    return version["__version__"]
+
+setup(
+    name="django-project",  # Replace with your actual project name
+    version=get_version(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+)

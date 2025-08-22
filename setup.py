8192e8e17ae59af95bf829b1b2319d42b737d18f
Nivethithachandrakrishnan <141031571+Nivethithachandru@users.noreply.github.com>
2025-08-22 15:02:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
-from setuptools import setup, find_packages
-
-def get_version():
-    version = {}
-    with open("core/__init__.py") as f:  # Use "demo/__init__.py" if using demo
-        exec(f.read(), version)
-    return version["__version__"]
-
 setup(
-    name="django-project",  # Replace with your actual project name
-    version=get_version(),
+    name="github-actions",
+    version="0.1.0",  
     packages=find_packages(),
-    include_package_data=True,
     install_requires=open("requirements.txt").read().splitlines(),
 )

956b3a4be94c291b422658a36ae51db3b0f5a1f6
Nivethithachandrakrishnan <141031571+Nivethithachandru@users.noreply.github.com>
2025-08-25 11:30:26 +0530

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -68,12 +68,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db.sqlite3',
+        'USER': 'Dviya',
+        'PASSWORD': 'Thulir@1910',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 if ENVIRONMENT == 'production':
     DEBUG = True
     SECRET_KEY = os.getenv('SECRET_KEY')
